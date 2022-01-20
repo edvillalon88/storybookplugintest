6170a41af7f6261de667dfe238e76e7dd16a1fcc
Eduardo Valdes <edvillalon88@gmail.com>
2022-01-20 12:39:28 -0600

    first commit

---
+++ stories/TestButton.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import Button from "../src/components/Button";
+
+export default {
+  title: "Example/TestButton",
+  component: Button,
+} as Meta;
